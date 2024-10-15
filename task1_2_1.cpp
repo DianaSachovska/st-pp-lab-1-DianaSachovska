3edde2728fc0d2a636437bf666c600b90e1529f0
DianaSachovska <91177694+DianaSachovska@users.noreply.github.com>
2024-10-15 23:46:38 +0300

    Create task1_2_1.cpp

---
+++ task1_2_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <Windows.h>
+#include <iterator>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+void Thread1() {
+    cout << "1\n";
+}
+
+void Thread2() {
+    cout << "2\n";
+}
+
+int main() {
+    thread t1(Thread1);
+    thread t2(Thread2);
+
+    t1.join();
+    t2.join();
+}
